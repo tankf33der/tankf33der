7ba872448879753100807046f7a5b82378a817d5
Mike <mpech@tuta.io>
2019-11-14 14:02:31 +0200

    0uxnas

--- bigtest/bigint.rb
+++ bigtest/bigint.rb
@@ -1,2 +1,35 @@
 
 puts("mike")
+#s = STDIN.gets.split(" ")
+#s[0] = s[0].to_i
+#puts s[0] + 100
+
+while s = STDIN.gets.split(" ")
+	s[0] = s[0].to_i
+	s[2] = s[2].to_i
+
+	case s[1]
+	when "+"
+		puts(s[0] + s[2])
+	when "-"
+		puts(s[0] - s[2])
+	when "*"
+		puts(s[0] * s[2])
+	when "/"
+		puts(s[0] / s[2])
+	when "%"
+		puts(s[0] % s[2])
+	when "&"
+		puts(s[0] & s[2])
+	when "|"
+		puts(s[0] | s[2])
+	when "xor"
+		puts(s[0] ^ s[2])
+	when "<<"
+		puts(s[0] << s[2])
+	when ">>"
+		puts(s[0] >> s[2])
+	end
+end
+
+

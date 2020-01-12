1ddc18322dcef42731562bc0bca1cfaa366aec19
Mike <mpech@tuta.io>
2020-01-12 13:35:57 +0200

    tm63765w

--- bigtest/bigint.d
+++ bigtest/bigint.d
@@ -2,14 +2,14 @@ import std.stdio;
 import std.string;
 import std.bigint;
 import std.conv;
- 
+
 void main() {
     foreach (line; stdin.byLine) {
         auto s = line.split(" ");
         BigInt a = s[0];
         if(s.length == 1) {
           writeln(a);
-          continue;        
+          continue;
         }
         BigInt b = s[2];
         switch(s[1]) {

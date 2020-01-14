6f29a54d8d36f6fa9f02c71c2406ed6e61e1fbac
Mike <mpech@tuta.io>
2020-01-14 14:36:35 +0200

    6nbf5z

--- utf8/generator.py
+++ utf8/generator.py
@@ -4,16 +4,19 @@ import sys
 
 def get_next_character(f):
     c = f.read(1)
+    i = 0
     while c: 
-        yield c
+        yield i,c
         c = f.read(1)
+        i = i + 1
  
 s = 0
 u = 0
 with open(sys.argv[1], encoding="utf-8") as f:
-    for c in get_next_character(f):
+    for i, c in get_next_character(f):
         s = s +ord(c)
         if c.isupper():
+           print(i, c)
            u = u + 1
            
 print(s)

c0d571351bb9a074224c1c7884e5b710eea729cb
Mike <mpech@tuta.io>
2020-01-15 19:48:14 +0200

    3uwg3y

---
+++ utf8-upp/README.md
@@ -0,0 +1,5 @@
+Lets create prototype to pass tests for (upp?) vs. python3 over latest Unicode.
+```
+$ pil generator.l > utf8-tables.l
+$
+```
--- utf8/generator.py
+++ utf8/generator.py
@@ -5,19 +5,18 @@ import sys
 def get_next_character(f):
     c = f.read(1)
     i = 0
-    while c: 
+    while c:
         yield i,c
         c = f.read(1)
         i = i + 1
- 
+
 s = 0
 u = 0
 with open(sys.argv[1], encoding="utf-8") as f:
     for i, c in get_next_character(f):
         s = s +ord(c)
         if c.isupper():
-           print(i, c)
            u = u + 1
-           
+
 print(s)
 print(u)

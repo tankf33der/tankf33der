cfcf1fb6ac74f7b77d659e54a905e7bbe8f3c878
Mike <mpech@tuta.io>
2020-02-24 12:58:34 +0200

    1ggqan

--- crypto/elligator-test.go
+++ crypto/elligator-test.go
@@ -1,1 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/agl/ed25519/extra25519"
+)
+
+func main() {
+    var pub, pub2, rep, prv [32]byte
+	fmt.Println("mike")
+    extra25519.ScalarBaseMult(&pub, &rep, &prv)
+    fmt.Println(prv)
+    fmt.Println(pub)
+    fmt.Println(rep)
+    extra25519.RepresentativeToPublicKey(&pub2, &rep)
+    fmt.Println(pub2)
+}
 

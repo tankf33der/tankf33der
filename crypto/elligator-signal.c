af3f9af6873f93f3a4a0dac0ed372e1e730cfaaa
Mike <mpech@tuta.io>
2020-02-24 17:29:28 +0200

    70z7q4

--- crypto/elligator-signal.c
+++ crypto/elligator-signal.c
@@ -1,11 +1,18 @@
+#include <stdio.h>
+
 #define fe_frombytes crypto_sign_ed25519_ref10_fe_frombytes
-void fe_frombytes(fe h,const unsigned char *s)
+#define fe_0 crypto_sign_ed25519_ref10_fe_0
+typedef int crypto_int32;
+typedef crypto_int32 fe[10];
+void fe_frombytes(fe h,const unsigned char *s);
+void fe_0(fe);
+
 
 int main(void) {
    unsigned char bytes[32];
    fe in, out;
 
-   for (i = 0; i < 32; i++) {
+   for (int i = 0; i < 32; i++) {
       bytes[i] = 0;
    }
    fe_0(in);

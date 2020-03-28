4e36831d964eae3b339dacbd45bce9406cc32a0f
Mike <mpech@tuta.io>
2020-03-28 18:54:12 +0200

    5y272um

--- micro-editor/init-v2.lua
+++ micro-editor/init-v2.lua
@@ -30,3 +30,21 @@ function init()
     config.TryBindKey("Alt-l", "lua:initlua.ixio", true)
 end
 
+--
+-- local shell = import("micro/shell")
+-- local config = import("micro/config")
+--
+-- function python3(bp)
+    -- local buf = bp.Buf
+    -- if buf:FileType() == "python" or buf:FileType() == "python3" then
+        -- if buf:Modified() then
+            -- bp:Save()
+        -- end
+        -- shell.RunInteractiveShell("python3 " .. buf.AbsPath, true, false)
+    -- end
+-- end
+--
+-- function init()
+    -- config.TryBindKey("F5", "lua:initlua.python3", true)
+-- end
+--
